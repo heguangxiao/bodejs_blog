7045f90ee66ad20d416a2760ae68c28ee89a2252
heguangxiao <heguangxiao@gmail.com>
2022-10-28 09:57:47 +0700

    ?

--- src/index.js
+++ src/index.js
@@ -30,6 +30,6 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 //route
 route(app);
 
-            app.listen(port, () => {
-                console.log(`Example app listening on port ${port}`);
-            });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
