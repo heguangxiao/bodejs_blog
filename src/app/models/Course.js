20b7465d4b3a9d8d9e92d04ad304ae5c98d1c2ab
heguangxiao <heguangxiao@gmail.com>
2022-11-04 17:12:00 +0700

    _

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
 
@@ -7,6 +8,7 @@ const ObjectId = Schema.ObjectId;
 
 const Course = new Schema(
     {
+        _id: Number,
         name: { type: String, require: true, maxLength: 255, default: '' },
         description: { type: String, maxLength: 600, default: '' },
         image: { type: String, maxLength: 255, default: '' },
@@ -15,11 +17,13 @@ const Course = new Schema(
         slug: { type: String, slug: 'name', unique: true },
     },
     {
+        _id: false,
         timestamps: true,
     },
 );
 
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
